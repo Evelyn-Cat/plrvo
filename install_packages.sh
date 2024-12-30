5e053795f8ed27dc28cf97b4275ae238f598f85e
Evelyn-Cat <qin.yang@uconn.edu>
2024-12-30 02:50:03 -0500

    check_accountant backup

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,35 @@
+# from https://github.com/awslabs/fast-differential-privacy/blob/main/requirements.txt
+# torch~=1.11.0+cu113
+# prv-accountant
+# transformers>=4.20.1
+# numpy
+# scipy
+# jupyterlab
+# jupyter
+# opacus>=1.0
+# ml-swissknife
+# opt_einsum
+# pytest
+# pydantic==1.10
+# tqdm>=4.62.1
+# deepspeed~=0.8.3
+# fairscale==0.4
+
+# mine
+pip install transformers
+pip install numpy
+pip install scipy
+pip install jupyterlab
+pip install jupyter
+pip install opacus==1.0
+pip install opt_einsum
+pip install pytest
+pip install pydantic==1.10
+pip install tqdm
+pip install fairscale==0.4
+pip install ml-swissknife
+pip install torchvision
 pip install torch==1.11.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
-transformers
-numpy
-scipy
+pip install prv-accountant
+pip install deepspeed
+
---
+++ plrvo_transformers/accountant/plrvo.py
@@ -0,0 +1,8 @@
+params = {
+    0.1: {
+        "a_G": 1,
+        "G_k": 5,
+        "G_theta": 0.1,
+        "distributions": ["Gamma"]
+    }
+}
