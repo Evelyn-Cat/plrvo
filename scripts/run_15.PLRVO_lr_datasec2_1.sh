04ed1fd68ec142d4418e067f11c7b88ffc7431aa
Evelyn-Cat <qin.yang@uconn.edu>
2024-12-31 18:00:16 -0500

    add dict update tool and some results

---
+++ scripts/run_15.PLRVO_lr_datasec2_1.sh
@@ -0,0 +1,22 @@
+# config_idx=14
+
+# noise_type=Gaussian
+# gpu_id=1
+# taskname=sst2
+# per_device_train_batch_size=150
+
+# bash run_config_idx_func_2models.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size
+# wait
+
+
+config_idx=18
+
+noise_type=PLRVO
+gpu_id=1
+per_device_train_batch_size=300
+lr=5e-5
+
+for taskname in sst2 qnli mnli qqp
+do
+    bash run_config_idx_func_lr_model2.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+done
