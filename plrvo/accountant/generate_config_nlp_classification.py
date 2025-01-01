c61016e65a1cb97053af3917f61e31ee9955a50a
Evelyn-Cat <qin.yang@uconn.edu>
2025-01-01 17:09:00 -0500

    more possible configs generated

--- plrvo/accountant/generate_config_nlp_classification.py
+++ plrvo/accountant/generate_config_nlp_classification.py
@@ -25,11 +25,11 @@ G0.columns = ["gaussian_eps", "gaussian_q", "gaussian_sigma", "clip", "gaussian_
 P0 = pd.read_csv(f"csv/{dataset}_Qn.csv")
 P0.columns = ["eps_check", "distortion_PLRV/C", "delta", "C", "q", "k", "theta", "T", "lambda_max", "matching_entries/cli"]
 
-print(G0)
-print(P0)
+# print(G0)
+# print(P0)
 
-print(P0.shape)
-print(G0.shape)
+# print(P0.shape)
+# print(G0.shape)
 # (1538, 10)
 # (19999, 5)
 
@@ -47,16 +47,16 @@ for jdx, fix_epsilon in enumerate(eps_list):
     P["dist_to_fix_eps"] = fix_epsilon - P["eps_check"]
     G["dist_to_fix_eps"] = fix_epsilon - G["gaussian_eps"]
 
-    P = P.sort_values(by='dist_to_fix_eps', ascending=True)
-    
-    for key, group in P.groupby('dist_to_fix_eps'):
-        top_P = group.nsmallest(1, 'distortion_PLRV/C').index
-        break
-    dict_P = P.loc[top_P[0], :].to_dict()
-    print(dict_P)
-    
+    P = P.sort_values(by='distortion_PLRV/C', ascending=True)
     
     try:
+        for key, group in P.groupby('dist_to_fix_eps'):
+            top_P = group.nsmallest(1, 'distortion_PLRV/C').index
+            break
+        kdx = P.loc[top_P, :].index.tolist()
+        dict_P = P.loc[top_P[0], :].to_dict()
+        print(dict_P)
+        
         G = G.sort_values(by='dist_to_fix_eps', ascending=True)
         for key, group in G.groupby('dist_to_fix_eps'):
             print(group)
@@ -65,18 +65,35 @@ for jdx, fix_epsilon in enumerate(eps_list):
         print(top_G[0])
         dict_G = G.loc[top_G[0], :].to_dict()
         print(dict_G)
+        dict_P["idx_P"] = kdx[0]
         dict_P["a_G"] = 1
         dict_P["distributions"] = ["Gamma"]
         dict_P.update(dict_G)
         dict_P['diff_distortion'] = dict_P["gaussian_dist/clip"] - dict_P["distortion_PLRV/C"]
+           
+        idx = int(jdx+confix_index_start[dataset])
+        if generate_dict:
+            with open(f"../configs/{idx}.json", "w") as json_file:
+                json.dump(dict_P, json_file, indent=4, ensure_ascii=False)
+        else:
+            print(dict_P)
     except:
-        dict_P["a_G"] = 1
-        dict_P["distributions"] = ["Gamma"]
-        
-    idx = int(jdx+confix_index_start[dataset])
+        pass
+     
+
+start_idx = idx
+P0 = P0.sort_values(by='distortion_PLRV/C', ascending=True)
+head_P0 = P0.head(20).index.tolist()
+print(head_P0)
+import random
+random_selection = random.sample(head_P0, 10)
+
+
+for jkdx, index_num in enumerate(random_selection):
+    row = P0.loc[index_num, :].to_dict()
+    
     if generate_dict:
-        with open(f"../configs/{idx}.json", "w") as json_file:
+        with open(f"../configs/{int(jkdx+start_idx)}.json", "w") as json_file:
             json.dump(dict_P, json_file, indent=4, ensure_ascii=False)
     else:
-        print(dict_P)
-
+        print(row)
