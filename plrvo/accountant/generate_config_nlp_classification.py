55aea7df3c9d5ebe16b048080df0f590ef18f11f
Evelyn-Cat <qin.yang@uconn.edu>
2025-01-01 17:15:20 -0500

    add index in the original PLRV solutions

--- plrvo/accountant/generate_config_nlp_classification.py
+++ plrvo/accountant/generate_config_nlp_classification.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 generate_dict=True
 dataset="sst-2"
-# dataset="qnli"
-# dataset="mnli"
-# dataset="qqp"
+dataset="qnli"
+dataset="mnli"
+dataset="qqp"
 confix_index_start = {
     "sst-2": 111,
     "qnli": 211,
@@ -65,7 +65,7 @@ for jdx, fix_epsilon in enumerate(eps_list):
         print(top_G[0])
         dict_G = G.loc[top_G[0], :].to_dict()
         print(dict_G)
-        dict_P["idx_P"] = kdx[0]
+        dict_P["idx_P"] = int(kdx[0])
         dict_P["a_G"] = 1
         dict_P["distributions"] = ["Gamma"]
         dict_P.update(dict_G)
@@ -92,6 +92,7 @@ random_selection = random.sample(head_P0, 10)
 
 for jkdx, index_num in enumerate(random_selection):
     row = P0.loc[index_num, :].to_dict()
+    rowp['idx'] = int(index_num)
     
     if generate_dict:
         with open(f"../configs/{int(jkdx+start_idx)}.json", "w") as json_file:
