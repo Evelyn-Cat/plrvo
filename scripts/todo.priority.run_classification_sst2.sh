b59a945c6d8191e291331620e22fcb07dcc902d4
Evelyn-Cat <qin.yang@uconn.edu>
2025-01-01 20:26:28 -0500

    run classification

--- scripts/todo.priority.run_classification_sst2.sh
+++ scripts/todo.priority.run_classification_sst2.sh
@@ -1,8 +1,20 @@
 # priority
-config_idx=118
-config_idx=117
+noise_type=PLRVO
+gpu_id=1
+taskname=sst2
+per_device_train_batch_size=170 # suggestion: memory 40G and running base model
+per_device_train_batch_size=96 # suggestion: memory 80G and running large model
 
 
-lr=5e-5
-lr=5e-6
-
+for config_idx in 118 117
+do
+    for lr in 5e-5 5e-6
+    do
+    # add & at the end of the line for parallel running.
+    bash run_config_idx_func_lr_bert_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+    # bash run_config_idx_func_lr_bert_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+    # bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+    # bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+    wait
+    done
+done
