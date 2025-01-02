80928605eb126e751e7d9ebfd892fdb804169bc0
Evelyn-Cat <qin.yang@uconn.edu>
2025-01-01 20:58:27 -0500

    this file is for jjl only too

---
+++ scripts/todo.priority.run_classification_mnli.jjl.sh
@@ -0,0 +1,107 @@
+# priority
+noise_type=PLRVO
+taskname=mnli
+
+## set 1
+
+config_idx=324
+per_device_train_batch_size=96 # suggestion: memory 80G and running large model
+
+gpu_id=0
+lr=5e-5
+bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=1
+lr=5e-6
+bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+    
+gpu_id=2
+lr=5e-5
+bash run_config_idx_func_lr_bert_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=3
+lr=5e-6
+bash run_config_idx_func_lr_bert_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+## set 2
+config_idx=324
+per_device_train_batch_size=170 # suggestion: memory 40G and running base model
+
+gpu_id=4
+lr=5e-5
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=5
+lr=5e-6
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+    
+gpu_id=6
+lr=5e-5
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=7
+lr=5e-6
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+wait
+
+
+
+## set 3
+config_idx=323
+per_device_train_batch_size=170 # suggestion: memory 40G and running base model
+
+gpu_id=4
+lr=5e-5
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=5
+lr=5e-6
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+    
+gpu_id=6
+lr=5e-5
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=7
+lr=5e-6
+bash run_config_idx_func_lr_roberta_base.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+wait
+
+
+
+
+## set 4
+config_idx=323
+per_device_train_batch_size=96 # suggestion: memory 80G and running large model
+
+gpu_id=0
+lr=5e-5
+bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=1
+lr=5e-6
+bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+    
+gpu_id=2
+lr=5e-5
+bash run_config_idx_func_lr_bert_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+gpu_id=3
+lr=5e-6
+bash run_config_idx_func_lr_bert_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+
+
+
+
+while true; do
+    gpu_id=7
+    noise_type=PLRVO
+    taskname=mnli
+    config_idx=324
+
+    lr=5e-6
+    per_device_train_batch_size=96 # suggestion: memory 80G and running large model
+    bash run_config_idx_func_lr_roberta_large.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr &
+done
