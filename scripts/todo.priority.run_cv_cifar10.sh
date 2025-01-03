3ad3e32e71e0c97817e6d418457735bc6593c394
qiy22005 <qin.yang@uconn.edu>
2025-01-02 20:47:36 -0500

    fix: cv model call error

--- scripts/todo.priority.run_cv_cifar10.sh
+++ scripts/todo.priority.run_cv_cifar10.sh
@@ -18,7 +18,7 @@ do
         per_device_train_batch_size=100
         # lr=5e-5
 
-        bash run_config_idx_func_lr.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+        bash run_config_idx_func_lr_vit.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
     done
 done
 
