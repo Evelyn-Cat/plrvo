e0da377f823f7ebc0c724dd47540b19d0d7008fb
Evelyn-Cat <qin.yang@uconn.edu>
2025-01-02 20:09:07 -0500

    cv

--- scripts/todo.priority.run_cv_cifar10.sh
+++ scripts/todo.priority.run_cv_cifar10.sh
@@ -7,3 +7,18 @@ config_idx=704
 # lr=5e-5
 # lr=5e-6
 
+
+for config_idx in 706 705 704
+do
+    for noise_type in PLRVO Gaussian
+    do
+        # noise_type=Gaussian
+        gpu_id=0
+        taskname=cifar10
+        per_device_train_batch_size=100
+        # lr=5e-5
+
+        bash run_config_idx_func_lr.sh $noise_type $gpu_id $config_idx $taskname $per_device_train_batch_size $lr
+    done
+done
+
